46584c84f0a37d1cb2cdeec2551ae4ecb290bffd
engabdiyare <abdifatah77789@gmail.com>
2024-11-29 00:05:37 +0300

    committed fashion web

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector("important");
+const hero = document.querySelector(".hero");
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#224f34";
+});
